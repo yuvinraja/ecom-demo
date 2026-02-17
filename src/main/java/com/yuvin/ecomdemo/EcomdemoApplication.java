4e12780e915291ebc09b7ad55f8a4fb2c58b7af0
yuvinraja <yuvinrajav@gmail.com>
2026-02-17 15:00:02 +0530

    Initial Commit

---
+++ src/main/java/com/yuvin/ecomdemo/EcomdemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuvin.ecomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuvin/ecomdemo/EcomdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuvin.ecomdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
