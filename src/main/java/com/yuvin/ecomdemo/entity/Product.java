aa6b6333836fe2313dc2b1688adeda1746dece53
yuvinraja <yuvinrajav@gmail.com>
2026-02-17 15:53:33 +0530

    Add @Table annotation to Product entity for database mapping

--- src/main/java/com/yuvin/ecomdemo/entity/Product.java
+++ src/main/java/com/yuvin/ecomdemo/entity/Product.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 @Entity
+@Table(name = "products")
 public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
