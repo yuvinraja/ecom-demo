2a3dc7ff1f5d8553fa3f0e48fd889a3de63c5753
yuvinraja <yuvinrajav@gmail.com>
2026-02-18 11:23:00 +0530

    Add ProductImage and ProductReview entities with validation constraints

---
+++ src/main/java/com/yuvin/ecomdemo/entity/ProductImage.java
@@ -0,0 +1,49 @@
+package com.yuvin.ecomdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProductImage {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String publicId;
+  private String url;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getPublicId() {
+    return publicId;
+  }
+
+  public void setPublicId(String publicId) {
+    this.publicId = publicId;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public ProductImage(Long id, String publicId, String url) {
+    super();
+    this.id = id;
+    this.publicId = publicId;
+    this.url = url;
+  }
+
+}
---
+++ src/main/java/com/yuvin/ecomdemo/entity/ProductReview.java
@@ -0,0 +1,54 @@
+package com.yuvin.ecomdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
+public class ProductReview {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Min(value = 1)
+  @Max(value = 5)
+  private Integer rating;
+
+  private String comment;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Integer getRating() {
+    return rating;
+  }
+
+  public void setRating(Integer rating) {
+    this.rating = rating;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public ProductReview(Long id, Integer rating, String comment) {
+    super();
+    this.id = id;
+    this.rating = rating;
+    this.comment = comment;
+  }
+
+}
