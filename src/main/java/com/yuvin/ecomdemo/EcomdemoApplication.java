9547827ac4e68e7ad7ae6370dd34c386e3038f1c
yuvinraja <yuvinrajav@gmail.com>
2026-02-20 15:58:26 +0530

    Add dotenv support for environment variables

--- src/main/java/com/yuvin/ecomdemo/EcomdemoApplication.java
+++ src/main/java/com/yuvin/ecomdemo/EcomdemoApplication.java
@@ -5,10 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
 public class EcomdemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./").ignoreIfMalformed().ignoreIfMissing().load();
+
+		if (dotenv.get("DB_URL") != null) {
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		}
+		if (dotenv.get("DB_USERNAME") != null) {
+			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		}
+		if (dotenv.get("DB_PASSWORD") != null) {
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		}
+
 		SpringApplication.run(EcomdemoApplication.class, args);
 	}
 
